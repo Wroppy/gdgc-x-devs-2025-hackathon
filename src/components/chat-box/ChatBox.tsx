b25279a7d38dca140ad56c4304e7d52ad1e744d9
Wroppy <wxwong806@gmail.com>
2025-07-19 11:01:26 +1200

    Added review form

---
+++ src/components/chat-box/ChatBox.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {}
+
+const ChatBox = (props: Props) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ChatBox
--- src/components/review-form/ReviewForm.tsx
+++ src/components/review-form/ReviewForm.tsx
@@ -1,24 +1,81 @@
-import { Button, Textarea } from "@mantine/core";
+import { Button, Center, Divider, Rating, Stack, Text, Textarea } from "@mantine/core";
 import React, { useState } from "react";
 import styles from "./review-form.module.css";
 
 type Props = {};
 
+const LabelContainer = ({
+  children,
+  text,
+  required = false,
+}: {
+  children: React.ReactNode;
+  text: string;
+  required?: boolean;
+}) => {
+  return (
+    <Stack className={styles.labelContainer} gap="sm">
+      <Text size="sm" fw={500}>
+        {text}
+        {required && <span className={styles.required}> *</span>}
+      </Text>
+      <Center>{children}</Center>
+    </Stack>
+  );
+};
+
 const ReviewForm = (props: Props) => {
   const [review, setReview] = useState("");
+  const [tasteRating, setTasteRating] = useState(0);
+  const [serviceRating, setServiceRating] = useState(0);
+  const [accuracyRating, setAccuracyRating] = useState(0);
+
   return (
     <form>
-      {/* Need to include stars */}
-      <Textarea
-        placeholder="E.g. This is a great restaurant! Highly recommend the pizza."
-        label="Write a review"
-        rows={4}
-        value={review}
-        onChange={(e) => setReview(e.target.value)}
-      />
-      <Button type="submit">
-        Submit Review
-      </Button>
+      <Stack>
+        <LabelContainer
+          required
+          text="How would you rate the taste of your food?"
+        >
+          <Rating
+            value={tasteRating}
+            onChange={setTasteRating}
+            fractions={2}
+            size="lg"
+          />
+        </LabelContainer>
+        <Divider />
+        <LabelContainer text="Service Rating" required>
+          <Rating
+            value={serviceRating}
+            onChange={setServiceRating}
+            fractions={2}
+            size="lg"
+          />
+        </LabelContainer>
+        <Divider />
+
+        <LabelContainer required text="Accuracy Rating">
+          <Rating
+            value={accuracyRating}
+            onChange={setAccuracyRating}
+            fractions={2}
+            size="lg"
+          />
+        </LabelContainer>
+        <Divider />
+
+        <Textarea
+          placeholder="E.g. This is a great restaurant! Highly recommend the pizza."
+          label="Write a review (optional)"
+          rows={4}
+          value={review}
+          onChange={(e) => setReview(e.target.value)}
+        />
+        <Button fullWidth color="green" type="submit">
+          Submit Review
+        </Button>
+      </Stack>
     </form>
   );
 };
