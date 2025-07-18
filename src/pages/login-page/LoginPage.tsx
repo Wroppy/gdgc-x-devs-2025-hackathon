628df36b89729f0c353d938f643af941de617e3e
Wroppy <wxwong806@gmail.com>
2025-07-19 11:45:02 +1200

    Changed theming again

--- src/pages/login-page/LoginPage.tsx
+++ src/pages/login-page/LoginPage.tsx
@@ -1,4 +1,4 @@
-import { Stack, Text } from "@mantine/core";
+import { Image, Stack, Text } from "@mantine/core";
 import React from "react";
 import LogInForm from "../../components/login-form/LogInForm";
 
@@ -7,8 +7,8 @@ type Props = {};
 const LoginPage = (props: Props) => {
   return <Stack>
     <div className="logoContainer">
-      <img
-        src="https://raw.githubusercontent.com/gdgc-x-devs-2025-hackathon/gdgc-x-devs-2025-hackathon/main/src/assets/logo.png"
+      <Image
+        src="/logo.png"
         alt="Logo"
         style={{ width: "100px", height: "100px" }}
       />
--- src/theme.ts
+++ src/theme.ts
@@ -1,21 +1,12 @@
-import { createTheme } from "@mantine/core";
+import { createTheme, type MantineColorsTuple } from "@mantine/core";
 
 const theme = createTheme({
   primaryColor: "orange",
   colors: {
-    orange: [
-      "#FF7A00",
-      "#FF7A00",
-      "#FF7A00",
-      "#FF7A00",
-      "#FF7A00",
-      "#FF7A00",
-      "#FF7A00",
-      "#FF7A00",
-      "#FF7A00",
-      "#FF7A00",
-      "#FF7A00",
-    ],
+    orange: Array.from(
+      { length: 10 },
+      (_, i) => `#E58135`
+    ) as unknown as MantineColorsTuple,
     accent: [
       "#00C58E",
       "#00C58E",
