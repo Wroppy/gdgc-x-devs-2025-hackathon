f144ab0139cd888b905ec6eb8e701508d6814f8f
Weyman Wong <wxwong806@gmail.com>
2025-07-18 23:14:58 +1200

    Added operating system context provider and wrapped it around root layout

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,12 +1,20 @@
+import { useOs } from "@mantine/hooks";
 import { Outlet } from "react-router";
+import { OSContext } from "../contexts/OperatingSystemContext";
 
 const RootLayout = () => {
+  const os = useOs();
+
   return (
     <div id="root-layout">
-      <div id="root-content">
-      <Outlet />
-        
-      </div>
+      <OSContext.Provider value={{
+        os,
+        isMobile: os === 'ios' || os === 'android'
+      }}>
+        <div id="root-content">
+          <Outlet />
+        </div>
+      </OSContext.Provider>
     </div>
   );
 };
