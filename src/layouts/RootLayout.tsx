4269e369d6e147fcf60b0a726263f92121d3fe22
Wroppy <wxwong806@gmail.com>
2025-07-18 20:49:44 +1200

    Set up initial boilerplate template

---
+++ src/layouts/RootLayout.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router";
+
+const RootLayout = () => {
+  return (
+    <div id="root-layout">
+      <div id="root-content">
+      <Outlet />
+        
+      </div>
+    </div>
+  );
+};
+
+export default RootLayout;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import RootLayout from "./layouts/RootLayout.tsx";
+import HomePage from "./pages/HomePage.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route element={<RootLayout />}>
+          <Route path="/" element={<HomePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
