cd54b4fccec46e842177f6067856561b00a25e99
Wroppy <wxwong806@gmail.com>
2025-07-19 19:29:39 +1200

    added some nice loading

--- src/pages/restaurant-find-requests/FindRequestsPage.tsx
+++ src/pages/restaurant-find-requests/FindRequestsPage.tsx
@@ -1,37 +1,123 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useAuth } from "../../contexts/AuthContext";
 import supabase from "../../supabase-client";
 import UserRequest from "./UserRequest";
 import type { CustomerRequest } from "../../types/CustomerRequest";
+import {
+  Box,
+  Center,
+  Loader,
+  Progress,
+  Select,
+  Skeleton,
+  Stack,
+  Text,
+} from "@mantine/core";
 
 type Props = {};
 
+const Skeletons = () => {
+  const placeholder = {
+    name: "Loading...",
+    id: 0,
+    group_size: 0,
+    created_at: new Date().toISOString(),
+    preferred_time: new Date().toISOString(),
+    notes: "",
+    preferred_cuisines: [],
+    preferred_vibes: [],
+    budget_lower: 0,
+    budget_upper: 0,
+  };
+  return (
+    <Skeleton>
+      <UserRequest customerRequest={placeholder} />
+    </Skeleton>
+  );
+};
+
 const FindRequestsPage = (props: Props) => {
   const { user, role } = useAuth();
+  const [loading, setLoading] = useState(true);
   console.log("User:", user);
   console.log("Role:", role);
 
   const [requests, setRequests] = useState<CustomerRequest[]>([]);
 
   useEffect(() => {
-    supabase.from("customer_requests").select("*").then(({data}) => {
-      setRequests(data as CustomerRequest[]);
-    })
-  }, [])
-
+    supabase
+      .from("customer_requests")
+      .select("*")
+      .then(({ data }) => {
+        if (!data) {
+          console.error("No data found");
+          setLoading(false);
+          return;
+        }
+        setRequests(data?.reverse() as CustomerRequest[]);
+        setLoading(false);
+      });
+  }, []);
 
   const s = supabase
     .from("customer_requests")
     .on("INSERT", (payload) => {
-      setRequests((prevRequests) => [...prevRequests, payload.new as unknown as any]);
+      setRequests((prevRequests) => [
+        payload.new as unknown as CustomerRequest,
+        ...prevRequests,
+      ]);
     })
     .subscribe();
 
-  return <div>
-    {requests.map((request) => (
-      <UserRequest key={request.id} customerRequest={request} />
-    ))}
-  </div>;
+  return (
+    <Stack>
+      <Stack p="lg">
+        <Text fw={500} size="lg">
+          Requests
+        </Text>
+        <Select
+          label="Sort by"
+          clearable
+          data={[
+            { value: "group-size", label: "Group Size" },
+            { value: "distance", label: "Distance" },
+            { value: "time-posted", label: "Time Posted" },
+          ]}
+          placeholder="Select sorting option"
+        />
+      </Stack>
+      <Box>
+        {loading ? (
+          <>
+            <Skeletons />
+            <Skeletons />
+            <Skeletons />
+            <Skeletons />
+            <Skeletons />
+          </>
+        ) : requests.length === 0 && !loading ? (
+          <Center>
+            <Stack>
+              <Text ta="center">No requests currently found.</Text>
+              <Text ta="center">
+                Don't worry, we are working hard to find more requests for you!
+              </Text>
+              <Center>
+                <Loader />
+              </Center>
+            </Stack>
+            <Box></Box>
+          </Center>
+        ) : requests.length === 0 ? (
+          <div>None!</div>
+        ) : (
+          requests.map((request) => (
+            <UserRequest key={request.id} customerRequest={request} />
+          ))
+        )}
+      </Box>
+    </Stack>
+  );
 };
 
 export default FindRequestsPage;
