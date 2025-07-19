6699c2f37d704f80af353a3b759918dd8bea2af8
Weyman Wong <wxwong806@gmail.com>
2025-07-20 00:48:56 +1200

    Added indicators

--- src/components/restaurant-offer-box/RestaurantOfferBox.tsx
+++ src/components/restaurant-offer-box/RestaurantOfferBox.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import type { Offer } from "../../types/Offer";
-import { Avatar, Box, Text } from "@mantine/core";
+import { Avatar, Box, Indicator, Text } from "@mantine/core";
 import styles from "./restaurant-offer-box.module.css";
 import { IconChevronRight } from "@tabler/icons-react";
 
@@ -10,7 +10,9 @@ const RestaurantOfferBox = ({ offer }: Props) => {
   const { name, offerMessage, avatarUrl } = offer;
   return (
     <div className={styles.restaurantOfferBox}>
-      <Avatar size="lg" radius="xl" src={avatarUrl} alt={name} />
+      <Indicator offset={7} color="accent" size={16}>
+        <Avatar size="lg" radius="xl" src={avatarUrl} alt={name} />
+      </Indicator>
       <Box>
         <Text size="xl" fw={500}>
           {name}
